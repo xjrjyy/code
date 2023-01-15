a6a24643b51f70ac98c5614bf6ef3fa95747de68
jrjyy <1479412322@qq.com>
2023-01-15 20:27:12 +0800

    add Atcoder filename

--- trans.py
+++ trans.py
@@ -7,6 +7,7 @@ RE_UOJ_URL = re.compile(r'.*uoj\.ac/problem/(\d+)')
 RE_LUOGU_URL = re.compile(r'.*luogu\.com\.cn/problem/([a-zA-Z]+\d+)')
 RE_CF_URL = re.compile(r'.*codeforces\.com/problemset/problem/(\d+)/([a-zA-Z])')
 RE_CF_GYM_URL = re.compile(r'.*codeforces\.com/gym/(\d+)/problem/([a-zA-Z])')
+RE_AT_URL = re.compile(r'.*atcoder\.jp/contests/a[brg]c\d{3}/tasks/(a[brg]c\d{3})_([1-9a-zA-Z])')
 
 RE_LUOGU_P_PROBLEM = re.compile(r'P(\d{4,})')
 
@@ -47,7 +48,8 @@ class ProblemInfo:
             return f'{str(self.oj)}_{self.contest}_{self.id}'
         if self.oj == OJType.CodeforcesGym:
             return f'{str(self.oj)}_{self.contest}_{self.id}'
-        # TODO: AT
+        if self.oj == OJType.Atcoder:
+            return f'{str(self.oj)}_{self.contest}_{self.id}'
         return 'Unknown'
 
 def parse_loj_pid(pid: str) -> Optional[ProblemInfo]:
@@ -62,17 +64,24 @@ def parse_luogu_pid(pid: str) -> Optional[ProblemInfo]:
     return None
 
 def parse_cf_pid(contest: str, pid: str) -> Optional[ProblemInfo]:
+    assert(len(pid) == 1)
     return ProblemInfo(OJType.Codeforces, pid, contest=contest)
 
 def parse_cf_gym_pid(contest: str, pid: str) -> Optional[ProblemInfo]:
+    assert(len(pid) == 1)
     return ProblemInfo(OJType.CodeforcesGym, pid, contest=contest)
 
+def parse_at_pid(contest: str, pid: str) -> Optional[ProblemInfo]:
+    assert(len(pid) == 1)
+    raw_id = pid if pid.isalpha() else chr(int(pid) - 1 + ord('a')) # arc001_1
+    return ProblemInfo(OJType.Atcoder, raw_id, contest=contest)
+
 def parse_url_info(url: str) -> Optional[ProblemInfo]:
-    # loj
+    # Loj
     if RE_LOJ_URL.match(url):
         pid, = RE_LOJ_URL.match(url).groups()
         return parse_loj_pid(pid)
-    # uoj
+    # Uoj
     if RE_UOJ_URL.match(url):
         pid, = RE_UOJ_URL.match(url).groups()
         return parse_uoj_pid(pid)
@@ -80,15 +89,18 @@ def parse_url_info(url: str) -> Optional[ProblemInfo]:
     if RE_LUOGU_URL.match(url):
         pid, = RE_LUOGU_URL.match(url).groups()
         return parse_luogu_pid(pid)
-    # CF
+    # Codeforces
     if RE_CF_URL.match(url):
         contest, pid = RE_CF_URL.match(url).groups()
         return parse_cf_pid(contest, pid)
-    # CF gym
+    # Codeforces Gym
     if RE_CF_GYM_URL.match(url):
         contest, pid = RE_CF_GYM_URL.match(url).groups()
         return parse_cf_gym_pid(contest, pid)
-    # TODO: AT
+    # Atcoder
+    if RE_AT_URL.match(url):
+        contest, pid = RE_AT_URL.match(url).groups()
+        return parse_at_pid(contest, pid)
     return None
 
 def url_to_filename(url: str):
@@ -101,8 +113,8 @@ def test():
         'https://www.luogu.com.cn/problem/P1001': 'luogu_p1001',
         'https://codeforces.com/problemset/problem/1/A': 'cf_1_a',
         'https://codeforces.com/gym/100001/problem/A': 'cf_gym_100001_a',
-        # 'https://atcoder.jp/contests/arc001/tasks/arc001_1': 'at_arc001_a',
-        # 'https://atcoder.jp/contests/arc058/tasks/arc058_a': 'at_arc058_a',
+        'https://atcoder.jp/contests/arc001/tasks/arc001_1': 'at_arc001_a',
+        'https://atcoder.jp/contests/arc058/tasks/arc058_a': 'at_arc058_a',
     }
     for (url, filename) in urls.items():
         info = parse_url_info(url)
@@ -111,5 +123,6 @@ def test():
         assert(filename == url_filename)
 
 if __name__ == '__main__':
+    test()
     url = input().strip()
     print(url_to_filename(url))
