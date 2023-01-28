28e4dda55b7ac3e487126309b881648a7c32ea06
jrjyy <1479412322@qq.com>
2023-01-28 13:13:45 +0800

    add tjoi2015_prob.cpp

---
+++ _contest/pts/tjoi/2015/tjoi2015_prob.cpp
@@ -0,0 +1,22 @@
+/* tjoi2015_prob.cpp */
+#include <bits/stdc++.h>
+
+#define fo(x) freopen(#x".in", "r", stdin); freopen(#x".out", "w", stdout);
+#define rep(i, l, r) for (int i = l, rep##i = r; i <= rep##i; ++i)
+#define per(i, r, l) for (int i = r, per##i = l; i >= per##i; --i)
+#define r0p(i, r) rep (i, 0, (r) - 1)
+#define p0r(i, l) per (i, (l) - 1, 0)
+#define each(x, s) for (auto &x : s)
+#define all(s) (s).begin(), (s).end()
+#define dbg(x) (cerr << "Line#" << __LINE__ << " " << #x << "=" << (x) << endl)
+
+using ll = long long; using ull = unsigned long long;
+template <class T> using vec = std::vector<T>; using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n; cin >> n;
+    cout << fixed << setprecision(10) << double(n) * (n + 1) / (double(2 * n - 1) * 2) << "\n";
+    // cerr << "\n" << double(clock()) / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
